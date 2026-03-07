7454bfcf8e30e3993c35c6588b5eb0cf4e5619d2
xl <adiggo@gmail.com>
2026-03-07 14:00:00 -0800

    Initial Commit

---
+++ bilibili_tvos/bilibili_tvosApp.swift
@@ -0,0 +1,17 @@
+//
+//  bilibili_tvosApp.swift
+//  bilibili_tvos
+//
+//  Created by Xiaoping Li on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct bilibili_tvosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bilibili_tvosTests/bilibili_tvosTests.swift
@@ -0,0 +1,17 @@
+//
+//  bilibili_tvosTests.swift
+//  bilibili_tvosTests
+//
+//  Created by Xiaoping Li on 3/7/26.
+//
+
+import Testing
+@testable import bilibili_tvos
+
+struct bilibili_tvosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
